ed1dda41701d285630342da484894bf5dcecca6e
Alyson Lima <alysonrafael2005@gmail.com>
2026-03-17 14:14:17 +0000

    Mudança em Upload

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,28 +1,15 @@
-import { useState } from 'react'
-
-export default function Upload({ setPage, setResult }) {
-  const [file, setFile] = useState(null)
-
-  const send = async () => {
-    const formData = new FormData()
-    formData.append('file', file)
-
-    const res = await fetch('https://classificador-alimentos-ai.onrender.com/predict', {
-      method: 'POST',
-      body: formData
-    })
-
-    const data = await res.json()
-    setResult(data)
-    setPage('result')
-  }
-
+function Upload({ setPage, setResult }) {
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h2>Enviar imagem</h2>
-      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-      <br /><br />
-      <button onClick={send}>Analisar</button>
+    <div>
+      <h2>Upload</h2>
+      <button onClick={() => {
+        setResult({ label: "Pizza", score: 0.95 })
+        setPage('result')
+      }}>
+        Simular Resultado
+      </button>
     </div>
   )
 }
+
+export default Upload
